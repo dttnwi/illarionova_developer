df334077f4d219438cb2c8b522beae23615974d3
Ekaterina <katerina.06.illarionoa@yandex.ru>
2024-12-26 16:40:12 +0300

    Круг

--- Circle.java
+++ Circle.java
@@ -1,5 +1,5 @@
-public class Circle {private double radius;
-
+public class Circle {
+    private double radius;
     public Circle(double radius) {
         if (radius <= 0) {
             throw new RuntimeException("There is no figure with such parameters.");
