9043ed60c3af90fe91c22c48ce247c9658aad97e
Ekaterina <katerina.06.illarionoa@yandex.ru>
2024-12-26 16:49:48 +0300

    студенты

---
+++ Students.java
@@ -0,0 +1,50 @@
+abstract class Student {
+    private String fullName;
+    private int course;
+    private double examScore;
+
+    public Student(String fullName, int course, double examScore) {
+        this.fullName = fullName;
+        this.course = course;
+        this.examScore = examScore;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public double getExamScore() {
+        return examScore;
+    }
+
+    public abstract void writeExam();
+}
+
+// Класс-наследник IUStudent
+class IUStudent extends Student {
+    public IUStudent(String fullName, int course, double examScore) {
+        super(fullName, course, examScore);
+    }
+
+    @Override
+    public void writeExam() {
+        System.out.println("IUStudent " + getFullName() + " is writing an exam for course " + getCourse());
+    }
+}
+
+// Класс-наследник MathStudent
+class MathStudent extends Student {
+    public MathStudent(String fullName, int course, double examScore) {
+        super(fullName, course, examScore);
+    }
+
+    @Override
+    public void writeExam() {
+        System.out.println("MathStudent " + getFullName() + " is writing an exam for course " + getCourse());
+    }
+}
+
