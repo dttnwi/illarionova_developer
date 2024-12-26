a1ccc22f90f31eeb910bec7ca7f35354adec90f1
Ekaterina <katerina.06.illarionoa@yandex.ru>
2024-12-26 16:32:36 +0300

    Максимальное число из 4 введенных

--- Main.java
+++ Main.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите четыре целых числа:");
+        System.out.print("Число 1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Число 2: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Число 3: ");
+        int num3 = scanner.nextInt();
+        System.out.print("Число 4: ");
+        int num4 = scanner.nextInt();
+        int max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+        if (num4 > max) {
+            max = num4;
+        }
+        System.out.println("Максимальное число: " + max);
+
+        scanner.close();
+    }
 }
